4e9386cc31781067a5bf78d02b23af8bd98f9928
itsparasbisht <iamparasbisht@gmail.com>
2024-06-23 20:03:08 +0530

    repo restructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+const envs = import.meta.env;
 
 function App() {
   return (
     <div>
       <h1>Vite app + Docker</h1>
+      <h2>{envs.VITE_APP_DUMMY_KEY}</h2>
     </div>
   );
 }
